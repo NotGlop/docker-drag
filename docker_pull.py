5bf523760dc010d11cfaa9a0065d082bec09e413
GuyOmer <6340924+GuyOmer@users.noreply.github.com>
2020-03-27 17:59:26 +0300

    Generate new token after each fetch (#15)
    * Generate new token after each fetch
    
    * Added support for Microsoft typo
    * Flushing ouptut for better verbosity
    
    * Updated the README to reflect improvements
    
    Co-authored-by: alireza <alirezadl.73@gmail.com>
    Co-authored-by: Guy Omer <a@a.com>
    Co-authored-by: NotGlop <26483750+NotGlop@users.noreply.github.com>

--- docker_pull.py
+++ docker_pull.py
@@ -48,11 +48,12 @@ if resp.status_code == 401:
 	except IndexError:
 		reg_service = ""
 
-
-# Get Docker token and fetch manifest v2 (this part is useless for unauthenticated registries like Microsoft)
-resp = requests.get('{}?service={}&scope=repository:{}:pull'.format(auth_url, reg_service, repository), verify=False)
-access_token = resp.json()['token']
-auth_head = {'Authorization':'Bearer '+ access_token, 'Accept':'application/vnd.docker.distribution.manifest.v2+json'}
+# Get Docker token (this function is useless for unauthenticated registries like Microsoft)
+def get_auth_head(registry):
+	resp = requests.get('{}?service={}&scope=repository:{}:pull'.format(auth_url, reg_service, repository), verify=False)
+	access_token = resp.json()['token']
+	auth_head = {'Authorization':'Bearer '+ access_token, 'Accept':'application/vnd.docker.distribution.manifest.v2+json'}
+	return auth_head
 
 # Docker style progress bar
 def progress_bar(ublob, nb_traits):
@@ -67,7 +68,8 @@ def progress_bar(ublob, nb_traits):
 	sys.stdout.write(']')
 	sys.stdout.flush()
 
-# Get image layer digests
+# Fetch manifest v2 and get image layer digests
+auth_head = get_auth_head(registry)
 resp = requests.get('https://{}/v2/{}/manifests/{}'.format(registry, repository, tag), headers=auth_head, verify=False)
 if (resp.status_code != 200):
 	print('[-] Cannot fetch manifest for {} [HTTP {}]'.format(repository, resp.status_code))
@@ -126,9 +128,10 @@ for layer in layers:
 	# Creating layer.tar file
 	sys.stdout.write(ublob[7:19] + ': Downloading...')
 	sys.stdout.flush()
+	auth_head = get_auth_head(registry) # refreshing token to avoid its expiration
 	bresp = requests.get('https://{}/v2/{}/blobs/{}'.format(registry, repository, ublob), headers=auth_head, stream=True, verify=False)
-	if (bresp.status_code != 200): # FIXME: if 401 (token expired), this will throw an error on 'urls' keyword
-		bresp = requests.get(layer['urls'][0], headers=auth_head, stream=True, verify=False) # will fail if token expired
+	if (bresp.status_code != 200): # When the layer is located at a custom URL
+		bresp = requests.get(layer['urls'][0], headers=auth_head, stream=True, verify=False)
 		if (bresp.status_code != 200):
 			print('\rERROR: Cannot download layer {} [HTTP {}]'.format(ublob[7:19], bresp.status_code, bresp.headers['Content-Length']))
 			print(bresp.content)
@@ -149,9 +152,10 @@ for layer in layers:
 					progress_bar(ublob, nb_traits)
 					acc = 0
 	sys.stdout.write("\r{}: Extracting ...{}".format(ublob[7:19], " "*50)) # Ugly but works everywhere
+	sys.stdout.flush()
 	with open(layerdir + '/layer.tar', "wb") as file: # Decompress gzip response
 		unzLayer = gzip.open(layerdir + '/layer_gzip.tar','rb')
-		file.write(unzLayer.read())
+		shutil.copyfileobj(unzLayer, file)
 		unzLayer.close()
 	os.remove(layerdir + '/layer_gzip.tar')
 	print("\r{}: Pull complete [{}]".format(ublob[7:19], bresp.headers['Content-Length']))
@@ -164,7 +168,10 @@ for layer in layers:
 		# FIXME: json.loads() automatically converts to unicode, thus decoding values whereas Docker doesn't
 		json_obj = json.loads(confresp.content)
 		del json_obj['history']
-		del json_obj['rootfs']
+		try:
+			del json_obj['rootfs']
+		except: # Because Microsoft loves case insensitiveness
+			del json_obj['rootfS']
 	else: # other layers json are empty
 		json_obj = json.loads(empty_json)
 	json_obj['id'] = fake_layerid
@@ -188,8 +195,10 @@ file.close()
 
 # Create image tar and clean tmp folder
 docker_tar = repo.replace('/', '_') + '_' + img + '.tar'
+sys.stdout.write("Creating archive ...")
+sys.stdout.flush()
 tar = tarfile.open(docker_tar, "w")
 tar.add(imgdir, arcname=os.path.sep)
 tar.close()
 shutil.rmtree(imgdir)
-print('Docker image pulled: ' + docker_tar)
+print('\rDocker image pulled: ' + docker_tar)
